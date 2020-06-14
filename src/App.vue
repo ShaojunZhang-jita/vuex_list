fb3757a0fa94c3d6c9019adf4ff4d6ec7ad827ad
ShaojunZhang <1416040357@qq.com>
2020-06-14 21:18:58 +0800

    提交项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,123 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="handleInputChange"/>
+    <a-button type="primary" @click="addItemToList">添加事项</a-button>
+    <a-list bordered :dataSource="infoList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked='item.done' @change="(e)=>{
+          cbStatusChanged(e, item.id)}">{{item.info}}</a-checkbox>
+        <!-- 删除链接  事件函数中的参数都是传递的id-->
+        <a slot="actions" @click="removeItemById(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDoneLength}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="viewKey === 'all'?'primary':'default'" @click="changList(all)">全部</a-button>
+          <a-button :type="viewKey === 'undone'?'primary':'default'" @click="changLis(undone)">未完成</a-button>
+          <a-button :type="viewKey === 'done'?'primary':'default'" @click="changList(done)">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a>清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// mapState是vuex中的一个模块
+// 它为一个函数放置在计算属性中 vuex中有state和getters的都需要调用mapState以及mapGetters
+import { mapState, mapGetters } from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+    // 清空了，按需导入从vuex中传递来的数组即可
+    }
+  },
+  created() {
+    this.$store.dispatch('getList')
+  },
+  //必须要用计算属性将state中的数据引用过来然后才可以使用 比如插值表达式以及动态绑定的属性中的数据等 比如此时viewKey可以发生变化了，就可以绑定属性了。
+  computed: {
+    // 将state中的数据映射过来 
+    ...mapState(['inputValue', 'viewKey']),
+    // getters中的成员进行映射过来 注意函数有返回值之后放置在数据中不用括号的
+    ...mapGetters(['infoList', 'unDoneLength'])
+  },
+  methods: {
+    handleInputChange(e) {
+      console.log(e.target.value);
+      // 调用vuex中的mutations的方法就是利用$store.dispath
+      this.$store.dispatch('setInputValue',e.target.value)
+      
+    },
+    // 像列表中新增一些数据
+    addItemToList() {
+      // 判断输入的值 注意输入的值 已经 又计算属性将其引入过来了
+      if(this.inputValue.trim().length<=0) {
+        // 则输入框为空 利用方法在窗口进行打印结果
+        let a = this.$message.warning('文本框内容为空');
+        return a;
+      }
+      // 调取mutations中的函数 即新增数据函数
+      this.$store.commit('addItem')
+    },
+    // 根据id删除对应的任务
+    removeItemById(id) {
+      // 验证一下id是否被传递
+      console.log(id);
+      // 调取mutation中的函数进行操作数据
+      this.$store.commit('removeItem',id)
+    },
+    // 复选框发生变化即勾选或者否的时候执行的函数 因为change是改变之后的状态，当点击复选框的时候，发生变化，这个时候状体也发生变化（有点模糊，暂时放置）
+    cbStatusChanged(e, id) {
+      // 通过e.target.checked可以得到最小的复选框状态
+      // console.log(e.target.checked);
+      // 打印得到哪一个复选框中的id 基本id都是从上面传递下来的
+      // console.log(id);
+      const param = {
+        id: id,
+        status: e.target.checked
+      }
+      // 触发mutations中的函数 第一个参数为调取的函数 第二个参数为传入的参数
+      this.$store.commit('changeStatus', param)
+      
+    },
+    // 给三个按钮绑定事件，传递参数值
+    changLis(key) {
+      // 打印出来看看
+      // console.log(key);
+      // 触发mutations中的事件改变vuex中的数据
+      this.$store('changeviewKey',key)
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
+
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,92 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        // 所有的任务列表都在这里存储
+        list: [],
+        // 文本框的默认值
+        inputValue: "111",
+        // 下一个id
+        nextId: 5,
+        viewKey: 'all'
+    },
+    // mutations来为state中的list赋值 其中的函数第一个为state，第二个为传入state中的值
+    mutations: {
+        initList(state, list) {
+            state.list = list;
+        },
+        // 为state中的inputValue赋值
+        setInputValue(state, val) {
+            state.inputValue = val;
+        },
+        addItem(state) {
+            // 因为要放入数组中 所以我们需要建立一个对象
+            const obj = {
+                id: state.nextId,
+                info: state.inputValue.trim(),
+                dont: false
+            }
+            state.list.push(obj)
+            state.nextId++
+                state.inputValue = ''
+        },
+        // 根据id传递参数
+        removeItem(state, id) {
+            // 根据id查找对应项的索引 其中x为每次查找中数组中的一项 i就是我找到的数组中的一项索引值 有个疑问 传递过来的不就是id值为什么还要查找呢。暂时放下
+            const i = state.list.findIndex(x => x.id === id)
+                // 删除数组的固定操作
+            if (i != -1) {
+                state.list(i, 1)
+            }
+        },
+        // 修改复选框的状态
+        changeStatus(state, parma) {
+            const i = state.list.findIndex(x => x.id === parma.id)
+            if (i !== -1) {
+                state.list[i].done = parma.status
+            }
+        },
+        // 清楚已完成的任务
+        cleanDone(state) {
+            state.list = state.list.filter(x => x.done === false)
+        },
+        // 修改视图的关键字
+        changeviewKey(state, key) {
+            state.viewKey = key
+        }
+    },
+    // actions中存放了是异步函数
+    actions: {
+        getList(context) {
+            axios.get('/list.json').then(({ data }) => {
+                console.log(data)
+                    // 通过context.commit来触发，mutations中的函数initList
+                    // data是会往list中传递的（第二个参数）
+                context.commit('initList', data)
+            })
+        }
+    },
+    // getters主要是操作已经保存的数据进行操作 对其进行包装 只显示特定包装的数
+    getters: {
+        // 统计未完成的任务的条数 对数组进行筛选才是关键问题
+        unDoneLength(state) {
+            return state.list.filter(x => x.done === false).length
+        },
+        // 过滤数组中的数据进而去展示到页面中
+        infoList(state) {
+            if (state.viewKey === 'all') {
+                return state.list
+            }
+            if (state.viewKey === 'undone') {
+                return state.list.filter(x => !x.done)
+            }
+            if (state.viewKey === 'done') {
+                return state.list.filter(x => x.done)
+            }
+            return state.list
+        }
+    }
 })
